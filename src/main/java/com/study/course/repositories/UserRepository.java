da6b3c9c13358756312050847b88864939eee57c
Edgar Andrade <blackstv11@gmail.com>
2026-02-11 14:05:33 -0300

    feat: JPA Repository

---
+++ src/main/java/com/study/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.study.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.study.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
