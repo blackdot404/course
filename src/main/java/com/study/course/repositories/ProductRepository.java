9dff6c5df1b857785ee5d213c8c3d6275b1d8c05
Edgar Andrade <blackstv11@gmail.com>
2026-02-12 11:54:08 -0300

    feat: Products

---
+++ src/main/java/com/study/course/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.study.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.study.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
